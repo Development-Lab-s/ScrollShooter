d21dacb6e0df7aa810c2f21612b665f710f10d09
Nugusaeyo <chsj0702@gmail.com>
2025-12-04 23:15:56 +0900

    SO Error Fix

--- Assets/Member/JYG/_Code/AfterEffector.cs
+++ Assets/Member/JYG/_Code/AfterEffector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace Member.JYG._Code
 {
@@ -39,6 +40,15 @@ namespace Member.JYG._Code
             changeVolume.weight = 1;
             yield return new WaitForSeconds(duration);
             StartCoroutine(EffectWeightDown());
+        }
+
+        public void VignettePosChangeToPlayer(Volume volume)
+        {
+            if (volume.profile.TryGet(out Vignette vignette))
+            {
+                Vector2 pos = new Vector2(Camera.main.WorldToViewportPoint(GameManager.Instance.Player.transform.position).x, 0.12f);
+                vignette.center.value = pos;
+            }
         }
 
     }
