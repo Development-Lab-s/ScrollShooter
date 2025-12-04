6e2c039dd4e76b16e172272e0e02f6e2a7bc265c
Hwanji203 <hwanji203@gmail.com>
2025-12-04 23:17:20 +0900

    etc

--- Assets/Member/Hwan/0002_Script/002_UI/01_GameOver/NestingOpener.cs
+++ Assets/Member/Hwan/0002_Script/002_UI/01_GameOver/NestingOpener.cs
@@ -1,9 +1,7 @@
 using csiimnida.CSILib.SoundManager.RunTime;
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class NestingOpener : MonoBehaviour
 {
