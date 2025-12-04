b696121adf21215d9c93f6607f7eddc9d9cefc73
Hwanji203 <hwanji203@gmail.com>
2025-12-05 00:39:35 +0900

    PlayerDeadEffect 다듬기

--- Assets/Member/Hwan/0002_Script/002_UI/02_Setting/SettingUI.cs
+++ Assets/Member/Hwan/0002_Script/002_UI/02_Setting/SettingUI.cs
@@ -14,6 +14,7 @@ public class SettingUI : MonoBehaviour, IUI
     public event Action<UIType> OnClose;
 
     public InteractiveType OpenInput => InteractiveType.Middle;
+
     public UIType UIType => UIType.SettingUI;
 
     public ValueSetter valueSetter;
@@ -29,6 +30,7 @@ public class SettingUI : MonoBehaviour, IUI
 
     public void Open()
     {
+
         Time.timeScale = 0;
         UIObject.SetActive(true);
         OnOpen?.Invoke(UIType);
@@ -65,17 +67,7 @@ public class SettingUI : MonoBehaviour, IUI
 
     public void RightMove() { }
 
-    public void MiddleMove()
-    {
-        if (UIObject.activeSelf == true)
-        {
-            Close();
-        }
-        else
-        {
-            Open();
-        }
-    }
+    public void MiddleMove() => Close();
 
     public void ScrollMove(int value) 
     {
